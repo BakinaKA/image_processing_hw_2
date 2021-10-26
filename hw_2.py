89eb71b39115d2dbef248ee2b636a05cc1d68314
BakinaKA <backina.xen@yandex.ru>
2021-10-26 23:39:36 +0300

    add 2 noise filters

--- hw_2.py
+++ hw_2.py
@@ -76,12 +76,23 @@ def CalcOfDamageAndNonDamage2(img_name, kernel_size):
     image = cv.imread(img_name)
     plt.imshow(image)
     plt.show()
+    
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(kernel_size,kernel_size))
     image_erode = cv.erode(image,kernel)
     plt.imshow(image_erode)
     plt.show()
-
-    hsv_img = cv.cvtColor(image_erode,cv.COLOR_BGR2HSV)
+    hsv_img1 = cv.cvtColor(image_erode,cv.COLOR_BGR2HSV)
+    
+    dst = cv.fastNlMeansDenoisingColored(image, None, kernel_size, kernel_size, 7, 21)
+    plt.imshow(dst)
+    plt.show()
+    hsv_img2 = cv.cvtColor(dst,cv.COLOR_BGR2HSV)
+    
+    mst = cv.medianBlur(image, kernel_size)
+    plt.imshow(mst)
+    plt.show()
+    hsv_img3 = cv.cvtColor(mst,cv.COLOR_BGR2HSV)
+    
     if img_name == "_test/1.jpg":
         markers = markers_for_1(image)
     if img_name == "_test/2.jpg":
@@ -95,25 +106,96 @@ def CalcOfDamageAndNonDamage2(img_name, kernel_size):
     if img_name == "_test/8.jpg":
         markers = markers_for_8(image)
     
-    leafs_area_BGR = cv.watershed(image_erode,markers)
-    healthy_part = cv.inRange( hsv_img,(36,25,25),( 86,255,255))
-    ill_part = leafs_area_BGR - healthy_part
-    mask = np.zeros_like(image,np.uint8)
-    mask[leafs_area_BGR > 1] = (255,0,255)
-    mask[ill_part > 1] = (0,0,255)
-    return mask
+    leafs_area_BGR1 = cv.watershed(image_erode,markers)
+    leafs_area_BGR2 = cv.watershed(dst,markers)
+    leafs_area_BGR3 = cv.watershed(mst,markers)
+    
+    healthy_part1 = cv.inRange( hsv_img1,(36,25,25),( 86,255,255))
+    ill_part1 = leafs_area_BGR1 - healthy_part1
+    mask1 = np.zeros_like(image,np.uint8)
+    mask1[leafs_area_BGR1 > 1] = (255,0,255)
+    mask1[ill_part1 > 1] = (0,0,255)
+    
+    healthy_part2 = cv.inRange( hsv_img2,(36,25,25),( 86,255,255))
+    ill_part2 = leafs_area_BGR2 - healthy_part2
+    mask2 = np.zeros_like(image,np.uint8)
+    mask2[leafs_area_BGR2 > 1] = (255,0,255)
+    mask2[ill_part2 > 1] = (0,0,255)
+    
+    healthy_part3 = cv.inRange( hsv_img3,(36,25,25),( 86,255,255))
+    ill_part3 = leafs_area_BGR3 - healthy_part3
+    mask3 = np.zeros_like(image,np.uint8)
+    mask3[leafs_area_BGR3 > 1] = (255,0,255)
+    mask3[ill_part3 > 1] = (0,0,255)
+    
+    if img_name == "_test/1.jpg":
+        cv.imwrite("_test/1_mod1.jpg",mask1)
+        cv.imwrite("_test/1_mod2.jpg",mask2)
+        cv.imwrite("_test/1_mod3.jpg",mask3)
+    if img_name == "_test/2.jpg":
+        cv.imwrite("_test/2_mod1.jpg",mask1)
+        cv.imwrite("_test/2_mod2.jpg",mask2)
+        cv.imwrite("_test/2_mod3.jpg",mask3)
+    if img_name == "_test/3.jpg":
+        cv.imwrite("_test/3_mod1.jpg",mask1)
+        cv.imwrite("_test/3_mod2.jpg",mask2)
+        cv.imwrite("_test/3_mod3.jpg",mask3)
+    if img_name == "_test/5.jpg":
+        cv.imwrite("_test/5_mod1.jpg",mask1)
+        cv.imwrite("_test/5_mod2.jpg",mask2)
+        cv.imwrite("_test/5_mod3.jpg",mask3)
+    if img_name == "_test/7.jpg":
+        cv.imwrite("_test/7_mod1.jpg",mask1)
+        cv.imwrite("_test/7_mod2.jpg",mask2)
+        cv.imwrite("_test/7_mod3.jpg",mask3)
+    if img_name == "_test/8.jpg":
+        cv.imwrite("_test/8_mod1.jpg",mask1)
+        cv.imwrite("_test/8_mod2.jpg",mask2)
+        cv.imwrite("_test/8_mod3.jpg",mask3)
+        
+    return (mask1,mask2,mask3)
 
-plt.imshow(CalcOfDamageAndNonDamage2("_test/1.jpg", 3))
+tmp = CalcOfDamageAndNonDamage2("_test/1.jpg", 3)
+plt.imshow(tmp[0])
 plt.show()
-plt.imshow(CalcOfDamageAndNonDamage2("_test/2.jpg", 7))
+plt.imshow(tmp[1])
 plt.show()
-plt.imshow(CalcOfDamageAndNonDamage2("_test/3.jpg", 3))
+plt.imshow(tmp[2])
 plt.show()
-plt.imshow(CalcOfDamageAndNonDamage2("_test/5.jpg", 5))
+tmp = CalcOfDamageAndNonDamage2("_test/2.jpg", 7)
+plt.imshow(tmp[0])
 plt.show()
-plt.imshow(CalcOfDamageAndNonDamage2("_test/7.jpg", 3))
+plt.imshow(tmp[1])
 plt.show()
-plt.imshow(CalcOfDamageAndNonDamage2("_test/8.jpg", 5))
+plt.imshow(tmp[2])
+plt.show()
+tmp = CalcOfDamageAndNonDamage2("_test/3.jpg", 3)
+plt.imshow(tmp[0])
+plt.show()
+plt.imshow(tmp[1])
+plt.show()
+plt.imshow(tmp[2])
+plt.show()
+tmp = CalcOfDamageAndNonDamage2("_test/5.jpg", 5)
+plt.imshow(tmp[0])
+plt.show()
+plt.imshow(tmp[1])
+plt.show()
+plt.imshow(tmp[2])
+plt.show()
+tmp = CalcOfDamageAndNonDamage2("_test/7.jpg", 3)
+plt.imshow(tmp[0])
+plt.show()
+plt.imshow(tmp[1])
+plt.show()
+plt.imshow(tmp[2])
+plt.show()
+tmp = CalcOfDamageAndNonDamage2("_test/8.jpg", 5)
+plt.imshow(tmp[0])
+plt.show()
+plt.imshow(tmp[1])
+plt.show()
+plt.imshow(tmp[2])
 plt.show()
 k = cv.waitKey(0)
 cv.destroyAllWindows()
